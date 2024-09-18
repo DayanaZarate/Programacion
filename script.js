83bc651b876f0531c8e7359f339e3cf112b8db57
Dayana <psicodayz19@gmail.com>
2024-09-18 16:10:26 -0500

    Arreglo

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 let jugador = 0
-let pc = 2
+let pc = 3
 jugador = prompt("Elige: 1 Piedra, 2 papel o 3 tijera")
 //alert(" Elegiste " + jugador)
 if(jugador == 1) {
-    alert("Elegiste piedra")
+    alert("Elegiste 🥌")
 }else if (jugador == 2) {
-    alert("Elegiste papel")
+    alert("Elegiste 📄")
 }else if (jugador == 3) {
-    alert("Elegiste tijera")
+    alert("Elegiste ✂")
 }else {
     alert("Perdiste")
 }
-    
-    
+if(pc == 1) {
+    alert("PC eligió 🥌")
+}else if (pc == 2) {
+    alert("PC eligió 📄")
+}else if (pc == 3) {
+    alert("PC eligió ✂")
+}
+if (pc == jugador) {
+    alert ("Empate")
+}else if (jugador == 1 && pc == 3) {
+    alert ("Ganaste")
+}else if (jugador == 2 && pc == 1) {
+    alert ("Ganaste")
+}else if (jugador == 3 && pc == 2) {
+    alert ("Ganaste")
+}else {
+    alert ("Perdiste")
+}
