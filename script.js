1857aa649f39a77b7936b027f31492cc2990a469
Dayana <psicodayz19@gmail.com>
2024-09-18 14:38:32 -0500

    Mi primer commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let jugador = 0
+let pc = 2
+jugador = prompt("Elige: 1 Piedra, 2 papel o 3 tijera")
+//alert(" Elegiste " + jugador)
+if(jugador == 1) {
+    alert("Elegiste piedra")
+}else if (jugador == 2) {
+    alert("Elegiste papel")
+}else if (jugador == 3) {
+    alert("Elegiste tijera")
+}else {
+    alert("Perdiste")
+}
+    
+    
